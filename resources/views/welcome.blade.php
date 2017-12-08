15cb47fa4a93dc6c8172b199f4f057ddec06f3d3
David Nyangi <davidnyangi@gmail.com>
2017-12-08 18:44:33 +0300

    Setup of Homepage with dummy data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,179 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
+        <meta name="description" content="Marangu Hills Schools">
+        <meta name="author" content="David Nyangi email me@davidnyangi.com">
+        <meta name="keywords" content="mha, marangu hills academy" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Marangu Hills Schools <> Welcome</title>
+        <link rel="icon" type="image/ico" href="http://www.maranguhillsschools.com/favicon.ico"/>
+        <link rel="stylesheet" type="text/css" href="{{asset('font-awesome-4.7.0/css/font-awesome.min.css')}}">
+        <link type="text/css" href="{{asset('css/materialize.min.css')}}" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link type="text/css" href="{{asset('css/app.css')}}" rel="stylesheet">
+        <link type="text/css" href="{{asset('css/animate.css')}}" rel="stylesheet">
+        <script type="text/javascript" src="{{asset('js/modernizr.custom.86080.js')}}"></script>
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+    <body id="page">
+        <ul class="cb-slideshow">
+            <li><span>Image 01</span><div><h3> ~ I Analyze ~</h3></div></li>
+            <li><span>Image 02</span><div><h3> ~ I Design ~</h3></div></li>
+            <li><span>Image 03</span><div><h3> ~ I Implement ~</h3></div></li>
+            <li><span>Image 04</span><div><h3> ~ I Test ~</h3></div></li>
+            <li><span>Image 05</span><div><h3> ~ I Deploy ~</h3></div></li>
+            <li><span>Image 06</span><div><h3> ~ And Finally, Maintain ~</h3></div></li>
+        </ul>
+        <header>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">MARANGU HILLS SCHOOLS</a>
+                    <ul id="menus" class="right hide-on-med-and-down">
+                        <li><a href="#">Domains</a></li>
+                        <li><a href="#">Hosting</a></li>
+                        <li><a href="#">Software Development</a></li>
+                        <li><a href="#">Premium Templates</a></li>
+                        <li><a href="#">Contacts</a></li>
+                        <li><i class="material-icons">search</i></li>
+                    </ul>
+                    <ul id="slide-out" class="side-nav">
+                        <li><div class="userView">
+                                <div class="background">
+                                    <img src="{{asset('images/1.jpeg')}}">
+                                </div>
+                                <a href="#!user"><img class="circle" src="{{asset('images/1.jpeg')}}"></a>
+                                <a href="#!name"><span class="white-text name">David Nyangi</span></a>
+                                <a href="#!email"><span class="white-text email">davy@davidnyangi.com</span></a>
+                            </div>
+                        </li>
+                        <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+                        <li><a href="#!">Second Link</a></li>
+                        <li><div class="divider"></div></li>
+                        <li><a class="subheader">Subheader</a></li>
+                        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+                    </ul>  
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </nav>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6 left">
+                    <div class="humb">
+                        <section class="animated zoomIn" data-wow-delay="1s" data-wow-iteration="10">
+                            <a href="#" data-activates="slide-out" class="wow slideInRight btn-floating button-collapse btn-small waves-effect waves-light facebook"><i class="material-icons">menu</i></a>
+                        </section>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col s12 m6 right">
+                        <div class="card small myc">
+                                <div class="card-content">
+                                <p>WEBSITE UNDER CONSTRUCTION <br/>Hello and thanks for stopping by...<br/>
+                                Welcome to my website. <br/>
+                                Feel free to navigate through my projects, bio, templates and many more!<br/>
+                                I use this site to host most of my projects, mobile especially.
+                                Otherwise, keep visiting for more and more updates!<br/>
+                                Thanks!</p>
+                                </div>
+                                <div class="card-action">
+                                    <div class="chip">
+                                        <img src="images/1.jpeg" alt="Contact Person">
+                                        Connect With Me: davidnyangi
+                                    </div>
+                                    <a href="https://www.facebook.com/davidnyangitz" class="btn-floating btn-small waves-effect waves-light facebook animated bounce" animation-duration="1s"><i class="fa fa-facebook fa-3x"></i></a>
+                                    <a href="https://twitter.com/davy_davy" class="btn-floating btn-small waves-effect waves-light twitter animated bounce" animation-duration="4s"><i class="fa fa-twitter fa-3x"></i></a>
+                                    <a href="https://www.instagram.com/_davy_d/?hl=en" class="btn-floating btn-small waves-effect waves-light instagram animated bounce" data-wow-delay="3s"><i class="fa fa-instagram fa-3x"></i></a>
+                                    <a href="https://www.linkedin.com/in/david-nyangi-591435138/" class="btn-floating btn-small waves-effect waves-light facebook animated bounce" data-wow-delay="4s"><i class="fa fa-linkedin fa-3x"></i></a>
+                                    <a href="https://www.google.co.tz/maps/place/Dar+es+Salaam/@-6.7696985,39.1142026,11z/data=!3m1!4b1!4m5!3m4!1s0x185c4bae169bd6f1:0x940f6b26a086a1dd!8m2!3d-6.792354!4d39.2083284?hl=en" class="btn-floating btn-small waves-effect waves-light facebook animated bounce" data-wow-delay="5s"><i class="material-icons">location_on</i></a>
+                                    <div class="fixed-action-btn vertical click-to-toggle animated pulse" data-wow-iteration="10">
+                                        <a class="btn-floating btn-large purple">
+                                        <i class="material-icons">menu</i>
+                                        </a>
+                                        <ul>
+                                            <li><a class="btn-floating red tooltipped" data-position="left" data-delay="10" data-tooltip="Contact Me"><i class="material-icons">message</i></a></li>
+                                            <li><a class="btn-floating cyan darken-1 tooltipped" data-position="left" data-delay="10" data-tooltip="Leave a Question"><i class="material-icons">live_help</i></a></li>
+                                            <li><a class="btn-floating green tooltipped" data-position="left" data-delay="10" data-tooltip="Did You Find What You're Looking For?"><i class="material-icons">visibility</i></a></li>
+                                            <li><a class="btn-floating violet tooltipped" data-position="left" data-delay="10" data-tooltip="Leave a Question"><i class="material-icons">location_on</i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            <div class="row">
+                <div class="col s12 m3">
+                    <div class="card adv">
+                        <div class="card-content white-text">
+                            <span class="card-title">About Me</span>
+                            <p>David A. Nyangi. <br/>Christian,<br/> 25 - 50 years old, University Graduate, Software Developer and Tanzanian!</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">VIEW MORE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m3">
+                    <div class="card adv">
+                        <div class="card-content white-text">
+                            <span class="card-title">Skills</span>
+                            <p>Material Design, Code Igniter, Laravel, Bootstrap, Joomla, WordPress, Visual Basic, Excel Macros(VBA), Java, JQuery, XML, MySQL, Shell Scripting etc.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">VIEW MORE</a> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m3">
+                    <div class="card adv">
+                        <div class="card-content white-text">
+                            <span class="card-title">What I Do</span>
+                            <p>I Develop Web and Mobile Applications for Both Companies and Individuals. Currently a Web and Mobile Developer at DTCS Company Limited
+                            </p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">VIEW MORE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m3">
+                    <div class="card adv">
+                        <div class="card-content white-text">
+                            <span class="card-title">Projects</span>
+                            <p>The links below will redirect you to different projects that I performed for companies, individuals and some my own!
+                            Feel free to view, comment and even download! 
+                            </p>
+                        </div>
+                        <div class="card-action">
+                            <a href="http://djsf.co.tz/system">Web</a>
+                            <a href="https://www.uplabs.com/davy_10_2">Mobile</a>
+                            <a href="https://www.dropbox.com/s/4pihvzcbyl7kjro/CIVE-ICT%20Dictionary.msi?dl=0">Desktop</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+                    <!-- <ul class="pagination left">
+                        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                        <li class="active"><a href="#!">1</a></li>
+                        <li class="waves-effect"><a href="#!">2</a></li>
+                        <li class="waves-effect"><a href="#!">3</a></li>
+                        <li class="waves-effect"><a href="#!">4</a></li>
+                        <li class="waves-effect"><a href="#!">5</a></li>
+                        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                    </ul> -->
         </div>
+        <script type="text/javascript" src="{{asset('js/jquery-2.2.3.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/tether.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/materialize.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/WOW.js')}}"></script>
+        <script>
+            $('.button-collapse').sideNav({
+                menuWidth: 300, 
+                edge: 'right', 
+                closeOnClick: true,
+                draggable: true 
+            }
+            );
+            new WOW().init();
+            $('.tooltipped').tooltip({delay: 50});
+        </script>
     </body>
 </html>
